61dd0d5cade3b10c67607807a9f45a4411d035a5
Alexandre Foncatti <afoncatti@enhesa.com>
2025-11-23 17:51:27 +0000

    Derptder

--- src/app/pages/InnergyRHPageComponent.ts
+++ src/app/pages/InnergyRHPageComponent.ts
@@ -5,7 +5,7 @@ import { ProjectNavigatorComponent } from '../components/projectNavigator';
   selector: 'page-innergy_rh',
   imports: [ProjectNavigatorComponent],
   template: `
-    <img src="/images/pages/innergy_rh_2x.png" alt="Innergy RH">
+    <img src="images/pages/innergy_rh_2x.png" alt="Innergy RH">
     <app-project-navigator
       [prevUrl]="'/portfolio/sebrae_canvas'"
       [nextUrl]="'/portfolio/precifique'"
--- src/app/pages/NotifymePageComponent.ts
+++ src/app/pages/NotifymePageComponent.ts
@@ -5,7 +5,7 @@ import { ProjectNavigatorComponent } from '../components/projectNavigator';
   selector: 'page-notifyme',
   imports: [ProjectNavigatorComponent],
   template: `
-    <img src="/images/pages/notifyme_2x.png" alt="Notifyme">
+    <img src="images/pages/notifyme_2x.png" alt="Notifyme">
     <app-project-navigator
       [prevUrl]="'/portfolio/wise'"
       [nextUrl]="'/portfolio/b4cred'"
